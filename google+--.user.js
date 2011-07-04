23d9b7f4bb72f1a863a9c6182930d9838e5c5b56
Micah Lee <micah@eff.org>
2011-07-04 00:07:56 -0700

    initial, and likely final, commit

---
+++ google+--.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name          Google+--
+// @namespace     https://github.com/micahflee
+// @description   Remove the Google+ shimmering icons from Google search results (pronounced "Google Plus Minus Minus")
+// @include       https://encrypted.google.com/*
+// @include       http://www.google.com/*
+// ==/UserScript==
+
+var style = document.createElement('style');
+style.innerHTML = '.esw { display:none; }';
+document.body.appendChild(style);
+
