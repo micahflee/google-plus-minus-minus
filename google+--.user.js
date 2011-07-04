ec25f4fe5742339fedd11749400020939aaad265
Micah Lee <micah@eff.org>
2011-07-04 00:23:10 -0700

    added support for chrome :)

--- google+--.user.js
+++ google+--.user.js
@@ -4,6 +4,8 @@
 // @description   Remove the Google+ shimmering icons from Google search results (pronounced "Google Plus Minus Minus")
 // @include       https://encrypted.google.com/*
 // @include       http://www.google.com/*
+// @match         https://encrypted.google.com/*
+// @match         http://www.google.com/*
 // ==/UserScript==
 
 var style = document.createElement('style');
